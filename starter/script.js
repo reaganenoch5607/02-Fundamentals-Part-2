41b2fe58e8628bc784bd950003b84d5e8d59775c
Reagan Enoch Owiti <68754800+reaganenoch5607@users.noreply.github.com>
2021-05-07 23:15:34 +0300

    Add files via upload

---
+++ starter/script.js
@@ -0,0 +1,27 @@
+// 'use strict';
+// function friuts(bananas, apples){
+// console.log(bananas, apples);
+// const goodies = `fruits with ${bananas} bananas and ${apples} apples.`
+// return friuts;
+// }
+
+// const bananasgoodies = friuts(7, 6);
+// console.log(bananasgoodies)
+
+// function calcage1(birthYear) {
+//     return 2021 - birthYear;
+// }
+// const age1 = calcage1(2002)
+// console.log(age1);
+
+//  const calcage2 = birthYear => 2021 - birthYear
+//   const age = calcage2(2002);
+//  console.log(age);
+
+// function calcage1 (birthYear, firstName){
+//     const age = 2037 - birthYear;
+//     console.log(`${firstName} is ${age} years old`);
+//     return age;
+// }
+// const age = calcage1 (1963, 'Reagan')
+
